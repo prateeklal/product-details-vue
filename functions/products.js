ffa6582e73b9b5837d1c552538df7665447e1f5b
Prateek Lal <lalprateek01@gmail.com>
2021-08-14 08:18:07 +0530

    added logs

--- functions/products.js
+++ functions/products.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
 
 exports.handler = async (event, context, callback) => {
+  console.log(event, '\n', context, '\n')
   let getResponse = await axios.get("https://www.westelm.com/services/catalog/v4/category/shop/new/all-new/index.json");
+  console.log(getResponse)
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(getResponse.data)
